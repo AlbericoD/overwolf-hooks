d0a4a10d7ee8389ec7414e1613baeb1859e65380
Albérico Dias Barreto Filho <AlbericoD@users.noreply.github.com>
2021-09-07 23:25:13 -0300

    update libs. fixed issue with useRunningGame

--- src/hooks/useRunningGame.test.ts
+++ src/hooks/useRunningGame.test.ts
@@ -1,15 +1,16 @@
-import "../overwolf.mock";
-import { useRunningGame } from "./useRunningGame";
-import { renderHook } from "@testing-library/react-hooks";
+import '../overwolf.mock'
+import { useRunningGame } from './useRunningGame'
+import { renderHook } from '@testing-library/react-hooks'
 
-describe("useRunningGame values", () => {
-  it("should return runningGame object", () => {
-    const { result } = renderHook(() => useRunningGame());
+describe('useRunningGame values', () => {
+  it('should return runningGame object', () => {
+    const { result } = renderHook(() => useRunningGame())
 
-    const [game] = result.current;
-    expect(game).not.toBeUndefined();
-    expect(game?.id).toBe(1000);
-    expect(game?.title).toBe("test-game_Test_TITLE");
-    expect(game?.gameRunning).toBe(true);
-  });
-});
+    const [game] = result.current
+    expect(game).not.toBeUndefined()
+    expect(game).not.toBeNull()
+    expect(game?.id).toBe(1000)
+    expect(game?.title).toBe('GAME TEST')
+    expect(game?.gameRunning).toBe(true)
+  })
+})
--- src/hooks/useRunningGame.ts
+++ src/hooks/useRunningGame.ts
@@ -1,35 +1,54 @@
-import { useState, useEffect, useCallback } from "react";
+import { useEffect, useState } from 'react'
+
 export interface UseRunningGamePayload {
-  gameRunning: boolean;
-  id: number;
-  title: string;
+  gameRunning: boolean
+  id: number
+  title: string
+  gameChanged: boolean
+  isInFocus: boolean
 }
+
 export const useRunningGame = () => {
-  const [game, setGame] = useState<UseRunningGamePayload>();
+  const [game, setGame] = useState<UseRunningGamePayload | null>(null)
 
   function onGameInfoUpdated(payload: overwolf.games.GameInfoUpdatedEvent) {
     const gameRunning: UseRunningGamePayload = {
-      gameRunning: payload.gameInfo?.isRunning || false,
-      id: Math.round((payload.gameInfo?.id || 0) / 10),
-      title: payload.gameInfo?.title || ""
-    };
-    const gameStateChanged = payload?.runningChanged || payload?.gameChanged;
-
-    if (gameStateChanged) setGame(gameRunning);
+      gameRunning: payload?.gameInfo?.isRunning ?? false,
+      id: Math.round((payload?.gameInfo?.id || 0) / 10),
+      title: payload?.gameInfo?.title || '',
+      gameChanged: payload?.gameChanged || false,
+      isInFocus: payload?.focusChanged || false,
+    }
+    setGame(gameRunning)
+    console.log(
+      '[onGameInfoUpdatedPayload] ',
+      JSON.stringify(gameRunning, null, 2),
+    )
   }
 
-  const addListener = useCallback(async () => {
-    overwolf.games.onGameInfoUpdated.removeListener(payload =>
-      onGameInfoUpdated(payload)
-    );
-    overwolf.games.onGameInfoUpdated.addListener(payload =>
-      onGameInfoUpdated(payload)
-    );
-  }, []);
+  function onGetRunningGameInfo(
+    payload: overwolf.games.GetRunningGameInfoResult,
+  ): void {
+    console.log('[onGetRunningGameInfo] ', JSON.stringify(payload, null, 2))
+    setGame((currentGame) => ({
+      gameChanged: currentGame?.gameChanged || false,
+      id: Math.round((payload?.id || 0) / 10),
+      title: payload?.title || '',
+      gameRunning: payload?.isRunning ?? false,
+      isInFocus: payload?.isInFocus ?? false,
+    }))
+  }
 
   useEffect(() => {
-    addListener();
-  }, [addListener]);
+    overwolf.games.getRunningGameInfo(onGetRunningGameInfo)
 
-  return [game] as const;
-};
+    overwolf.games.onGameInfoUpdated.removeListener(onGameInfoUpdated)
+    overwolf.games.onGameInfoUpdated.addListener(onGameInfoUpdated)
+
+    return () => {
+      overwolf.games.onGameInfoUpdated.removeListener(onGameInfoUpdated)
+    }
+  }, [])
+
+  return [game] as const
+}
